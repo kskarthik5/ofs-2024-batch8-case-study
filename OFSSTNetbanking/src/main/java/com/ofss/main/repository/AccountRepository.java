ab2743ff5f09a78ef8c8b2cbca44ff9c8ec25185
Karthik Kaimaparambil <kskarthik20025@gmail.com>
2024-08-14 10:01:05 +0530

    Webservices for Account

--- OFSSTNetbanking/src/main/java/com/ofss/main/repository/AccountRepository.java
+++ OFSSTNetbanking/src/main/java/com/ofss/main/repository/AccountRepository.java
@@ -1,14 +1,17 @@
 package com.ofss.main.repository;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ofss.main.domain.Account;
-import com.ofss.main.domain.Customer;
 
 public interface AccountRepository extends CrudRepository<Account, Integer>{
 	@Procedure(name="createAccount")
 	Map<String,Object> createAccount(int customerId,String accountType);
+	@Query("SELECT a from Account a WHERE a.customer.customerId=?1")
+	List<Account> findByCustomerId(int customerId);
 }
